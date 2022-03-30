dfad0822dceac4fca283c539ba6953cf2e75bb0d
nghiatrinh107 <nghiatrinh107@gmail.com>
2022-03-30 16:29:18 +0700

    nop bai ss3

--- ss3_thao_tac_voi_csdl/bai_tap/quan_li_ban_hang.sql
+++ ss3_thao_tac_voi_csdl/bai_tap/quan_li_ban_hang.sql
@@ -18,9 +18,10 @@ product_name varchar(50),
 product_price double
 );
 create table order_detail(
-order_id int,
-product_id int,
-od_qty varchar(100),
+order_id int ,
+product_id int ,
+od_qty int,
+primary key (order_id,product_id),
 foreign key (order_id) references `order`(order_id),
 foreign key (product_id) references product(product_id)
 );
@@ -39,4 +40,10 @@ insert into quan_li_ban_hang.order_detail
 values
 (1,1,'3'),(1,3,'7'),(1,4,'2'),(2,1,'1'),(3,1,'8'),(2,5,'4'),(2,3,'3');
 SELECT order_id,order_date,order_total_price FROM quan_li_ban_hang.order;
-select customer_name,product_name from
+select customer.*,product_name from customer join `order` on customer.customer_id = `order`.customer_id 
+inner join  order_detail on `order`.order_id = order_detail.order_id
+inner join product on order_detail.product_id = product.product_id;
+select customer.customer_name from customer left join `order` on customer.customer_id = `order`.customer_id where `order`.customer_id is null;
+select `order`.order_id,order_date,sum(od_qty*product_price) as total from  `order` inner join  order_detail on `order`.order_id = order_detail.order_id
+inner join product on order_detail.product_id = product.product_id
+group by `order`.order_id;
